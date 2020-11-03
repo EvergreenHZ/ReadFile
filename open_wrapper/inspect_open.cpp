66998cafbbf52ad9e2cbed443b21cdab054e80bc
EvergreenHZ <huaizhixtp@163.com>
2020-11-03 16:05:16 +0000

    add backtrace

--- open_wrapper/inspect_open.cpp
+++ open_wrapper/inspect_open.cpp
@@ -1,11 +1,35 @@
 #include <dlfcn.h>
 #include <stdio.h>
+#include <execinfo.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 typedef int (*open_ptr)(const char*, int flags);
 
+void print_backtrace()
+{
+        static const int STACK_DEPTH = 40;
+        int nptrs = 0;
+        void *buffer[STACK_DEPTH];
+        char **strings;
+
+        nptrs = backtrace(buffer, STACK_DEPTH);
+
+        strings = backtrace_symbols(buffer, nptrs);
+        if (strings != NULL) {
+                for (int i = 0; i < nptrs; i++) {
+                        printf("%s\n", strings[i]);
+                }
+        }
+}
+
+
 extern "C" int open(const char* pathname, int flags, ...)
 {
+
         open_ptr ori_open = (open_ptr)dlsym(RTLD_NEXT, "open");
-        printf("%s\n", pathname);
+        print_backtrace();
+        //printf("%s\n", pathname);
         return ori_open(pathname, flags);
 }
